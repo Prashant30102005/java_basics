4d6f4fa6a6e2f8034927f961e02a70594df294c7
Prashant30102005 <prashantparihar638@gmail.com>
2025-06-21 21:09:00 +0530

    Swap

---
+++ Day 1/Swap.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Swap{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a =s.nextInt();
+        System.out.println("Enter second number");
+        int b =s.nextInt();
+        int temp = a;
+        a=b;
+        b=temp;
+        System.out.println("the first number: "+a);
+        System.out.println("the second number: "+b);
+                
+    }    
+}
