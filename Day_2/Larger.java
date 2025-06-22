d629404676f87d613cfb5bf9a871d4d7d828cfa9
Prashant30102005 <prashantparihar638@gmail.com>
2025-06-23 00:05:59 +0530

    large among three number

---
+++ Day_2/Larger.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Larger {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int a =s.nextInt();
+        int b =s.nextInt();
+        int c =s.nextInt();
+        
+        
+        if (a>b&&a>c){
+            System.out.println("A is largest");
+        }
+        else if(b>c&&b>a){
+            System.out.println("b is largest");
+        }
+        else{
+            System.out.println("c is largest");
+        }
+
+    }
+}
