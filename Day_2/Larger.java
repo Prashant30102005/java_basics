3533b941eafa16f0f06656e16f160f6741680e20
Prashant30102005 <prashantparihar638@gmail.com>
2025-06-23 18:12:09 +0530

    day_2

--- Day_2/Larger.java
+++ Day_2/Larger.java
@@ -1,26 +1,22 @@
 package Day_2;
 
 import java.util.Scanner;
+
 public class Larger {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("enter first number");
-        int a =s.nextInt();
+        int a = s.nextInt();
         System.out.println("enter second number");
-        int b =s.nextInt();
+        int b = s.nextInt();
         System.out.println("enter third number");
-        int c =s.nextInt();
-        
-        
-        if (a>b&&a>c){
+        int c = s.nextInt();
+        if (a > b && a > c) {
             System.out.println("First is largest");
-        }
-        else if(b>c&&b>a){
+        } else if (b > c && b > a) {
             System.out.println("Second is largest");
-        }
-        else{
+        } else {
             System.out.println("Third is largest");
         }
-
     }
 }
